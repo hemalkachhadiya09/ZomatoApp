6e0fb16d4952d2652b818527dd455e12621905ea
Hemalkachhadiya09 <kachhadiyahemal3@gmail.com>
2022-07-12 22:07:52 +0530

    "code"

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function closeCoupon()
 {
     document.getElementById("Coupon").style.display="none"
 }
+function demoExternal()
+{
+    if(confirm('Are you sure for logout  from the web?'))
+    {
+        alert('You are logged out')
+    }
+    else
+    {
+        alert('Try!!sorry you can\'t logged out' )
+    }
+}
